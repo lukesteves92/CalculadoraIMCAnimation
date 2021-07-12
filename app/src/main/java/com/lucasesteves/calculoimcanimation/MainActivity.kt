2b89a3b412d0240b6f6624cb23ef9c8a7d4e94e6
Lucas Esteves <lucas.coelho9280@gmail.com>
2021-07-12 13:01:04 -0300

    first commit

---
+++ app/src/main/java/com/lucasesteves/calculoimcanimation/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.lucasesteves.calculoimcanimation
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.Navigation
+import com.lucasesteves.calculoimcanimation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
